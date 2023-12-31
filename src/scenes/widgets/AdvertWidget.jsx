8239a5355957e13d60089314e37bc31882d207aa
s13p <142729653+s13p@users.noreply.github.com>
2023-12-31 12:14:34 +0300

    Update AdvertWidget.jsx

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -12,9 +12,9 @@ const AdvertWidget = () => {
     <WidgetWrapper>
       <FlexBetween>
         <Typography color={dark} variant="h5" fontWeight="500">
-          Sponsored
+        
         </Typography>
-        <Typography color={medium}>Create Ad</Typography>
+        <Typography color={medium}></Typography>
       </FlexBetween>
       <img
         width="100%"
@@ -24,12 +24,11 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}></Typography>
+        <Typography color={medium}></Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+       Made by yours truly
       </Typography>
     </WidgetWrapper>
   );
