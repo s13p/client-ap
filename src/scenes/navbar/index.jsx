8a347d4e0807945f832bc5e48b45276be96c0439
s13p <142729653+s13p@users.noreply.github.com>
2023-12-31 11:18:33 +0300

    UKHK

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          APOTHOPOTHO
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
